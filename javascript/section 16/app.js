ccd2568eeba48f8b944c1a0d3db7dfbf17b4bddc
dlthdnjs0517 <thdnjs0517@naver.com>
2025-03-27 15:11:23 +0900

    new file:   javascript/section 16/app.js 	new file:   javascript/section 16/index.html

---
+++ javascript/section 16/app.js
@@ -0,0 +1,9 @@
+let ran = Math.random()
+if (ran < 0.5) {
+	console.log("your number is under 0.5")
+	console.log(ran)
+}
+if (ran >= 0.5) {
+	console.log("your number is greater than 0.5")
+	console.log(ran)
+}
