a9ed4e2488f626796ad2030c6c0683b5f24a05a6
dlthdnjs0517 <thdnjs0517@naver.com>
2025-03-31 23:51:08 +0900

    final version_todolist

--- javascript/to_do_list_Project/todos.js
+++ javascript/to_do_list_Project/todos.js
@@ -1,13 +1,10 @@
 let choice;
-let addlist = [];
+const addlist = [];
 while (choice !== 'quit') {
 	choice = prompt("What would you like to do?");
-	if (choice === 'quit') {
-		break;
-	}
-	else if (choice === 'new') {
+	if (choice === 'new') {
 		addlist.push(prompt("Enter new to do"));
-		console.log(`${addlist} is added to list`);
+		console.log(`${addlist[addlist.length - 1]} is added to list`);
 	} else if (choice === 'list') {
 		console.log("**********")
 		for (let i = 0; i < addlist.length; i++) {
@@ -16,9 +13,6 @@ while (choice !== 'quit') {
 		console.log("**********")
 	} else if (choice === 'delete') {
 		indexNum = parseInt(prompt("Enter a index of todo to delete"));
-		while (!indexNum) {
-			console.log("enter a valid index number ")
-		}
 		addlist.splice(indexNum, 1);
-	}
+	} else if (choice === 'quit') break;
 }
