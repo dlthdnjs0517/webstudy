a309b2707da4b57d6be0c72ba2264c2eb2d8121d
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-01 15:58:03 +0900

    강의듣고 보충한 코드

--- javascript/to_do_list_Project/todos.js
+++ javascript/to_do_list_Project/todos.js
@@ -1,10 +1,12 @@
-let choice;
+let choice = prompt('What would you like to do?');
 const addlist = [];
-while (choice !== 'quit') {
-	choice = prompt("What would you like to do?");
+while (choice !== 'quit' && choice !== 'q') {
 	if (choice === 'new') {
-		addlist.push(prompt("Enter new to do"));
-		console.log(`${addlist[addlist.length - 1]} is added to list`);
+		const newTodo = prompt('what is new to do?');
+		addlist.push(newTodo);
+		console.log(`${newTodo} is added to the list`);
+		// addlist.push(prompt("Enter new to do"));
+		// console.log(`${addlist[addlist.length - 1]} is added to list`);
 	} else if (choice === 'list') {
 		console.log("**********")
 		for (let i = 0; i < addlist.length; i++) {
@@ -12,7 +14,14 @@ while (choice !== 'quit') {
 		}
 		console.log("**********")
 	} else if (choice === 'delete') {
-		indexNum = parseInt(prompt("Enter a index of todo to delete"));
-		addlist.splice(indexNum, 1);
-	} else if (choice === 'quit') break;
+		const indexNum = parseInt(prompt("Enter a index of todo to delete"));
+		if (!Number.isNaN(indexNum)) {
+			const deleted = addlist.splice(indexNum, 1);
+			console.log(`ok, deleted ${deleted[0]}`);
+		} else {
+			console.log('Unknown Index');
+		}
+	}
+	choice = prompt('What would you like to do?');
 }
+console.log('ok quit the app!');
