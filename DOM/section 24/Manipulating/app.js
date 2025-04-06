5449aa05e4ec969008b8b6394e4b912c65671151
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-06 21:42:52 +0900

    setAttribute 완료

--- DOM/section 24/Manipulating/app.js
+++ DOM/section 24/Manipulating/app.js
@@ -1,37 +1,29 @@
-const allImages = document.getElementsByTagName('img');
+// const allLinks = document.querySelectorAll('a');
 
-// for (let img of allImages) {
-//     console.log(img.src)
+// for (let link of allLinks) {
+// 	link.innerText = 'I AM A LINK!!'
 // }
-// 모든 img의 src 값이 출력됨
 
-// for (let img of allImages) {
-//     img.src = 'https://plus.unsplash.com/premium_photo-1709901922298-0a3eb89b7e79?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-// }
-//모든 사진이 바뀜
+const h1 = document.querySelector('h1')
+// document.querySelector('h1').innerText = '<i>asdfasf</i>'
+//<i>asdfasf</i>가 h1요소로 그대로 나타남,<i>태그를 텍스트로 인식해서 저렇게 됨
 
-// const squareImages = document.getElementsByClassName('square');
-// for (let img of squareImages) {
-//     img.src = 'https://images.unsplash.com/photo-1559622214-f8a9850965bb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D'
-// }
-//1) tag 이름으로 검색하는 방법
-// document.querySelector('p');
-//가장 처음으로 일치하는 값을 리턴한다.(실제로는 tag'p'는 여러개)
+// document.querySelector('p').innerHTML
+// 결과값에 모든 html 태그요소 및 컨텐츠들이 표시됨.(아래 참고)
+//'The <b>Silkie</b> (sometimes spelled <b>Silky</b>)
+//  is a <a href="/wiki/List_of_chicken_breeds" 
+// title="List of chicken breeds">breed</a> of 
+// <a href="/wiki/Chicken" title="Chicken">chicken</a> named for\n
 
-//2) id 이름으로 선택하는 방법
-// document.querySelector('#banner')
-// <img id="banner" src="https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2550&amp;q=80" alt="">
+// document.querySelector('h1').innerHTML = '<i>asdfasdf</i>'
+//line 7의 결과와 달리 <i></i>가 적용된 asdfasdf가 나옴.(html임을 인식)
 
-//3) class 이름으로 선택하는 방법
-// document.querySelector('.square');
+// document.querySelector('h1').innerHTML += '<sup>asdfasdf</sup>'
+//+= 연산자도 적용가능
 
-// 2번째 요소를 선택하는 방법
-// document.querySelector('img : nth-of-type(2)')
-//square 라는 클래스를 지닌 두번째 이미지의 정보 출력
+// document.querySelector('#banner').id = 'Whoops'
+//id를 변경함으로써 이미지에 적용되어있는 id 속성에 영향을 줌(사이즈 조절됨)
 
-//4) querySelectorAll
-const links = document.querySelectorAll('p a');
-for (let link of links) {
-    console.log(link.href)
-}
-//paraghraph 안에 있는 모든 anchor tag의 출처들을 출력
+//attribute 변경법:
+
+//document.querySelector('a').setAttribute('변경속성','변경할 내용')
--- DOM/section 24/Selecting/app.js
+++ DOM/section 24/Selecting/app.js
@@ -1,18 +1,37 @@
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+// for (let img of allImages) {
+//     console.log(img.src)
+// }
+// 모든 img의 src 값이 출력됨
 
+// for (let img of allImages) {
+//     img.src = 'https://plus.unsplash.com/premium_photo-1709901922298-0a3eb89b7e79?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+// }
+//모든 사진이 바뀜
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
+// for (let img of squareImages) {
+//     img.src = 'https://images.unsplash.com/photo-1559622214-f8a9850965bb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D'
+// }
+//1) tag 이름으로 검색하는 방법
+// document.querySelector('p');
+//가장 처음으로 일치하는 값을 리턴한다.(실제로는 tag'p'는 여러개)
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
+//2) id 이름으로 선택하는 방법
+// document.querySelector('#banner')
+// <img id="banner" src="https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2550&amp;q=80" alt="">
 
+//3) class 이름으로 선택하는 방법
+// document.querySelector('.square');
+
+// 2번째 요소를 선택하는 방법
+// document.querySelector('img : nth-of-type(2)')
+//square 라는 클래스를 지닌 두번째 이미지의 정보 출력
+
+//4) querySelectorAll
 const links = document.querySelectorAll('p a');
-
 for (let link of links) {
     console.log(link.href)
 }
+//paraghraph 안에 있는 모든 anchor tag의 출처들을 출력
