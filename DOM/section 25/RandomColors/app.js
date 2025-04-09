d5502b2b2eb523d1087b8692df19de7fa8dfdfe1
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-09 15:32:39 +0900

    random color 보완, this 파일 추가

--- DOM/section 25/RandomColors/app.js
+++ DOM/section 25/RandomColors/app.js
@@ -4,7 +4,19 @@ btn.addEventListener('click', () => {
 	const newColor = makeRandomColor();
 	document.body.style.backgroundColor = newColor;
 	h1.innerText = newColor;
-})
+	const rgbValues = newColor.match(/\d+/g);
+	const r = parseInt(rgbValues[0]);
+	const g = parseInt(rgbValues[1]);
+	const b = parseInt(rgbValues[2]);
+
+	// 밝기 계산: r + g + b 값이 400보다 작으면 텍스트 색을 흰색으로 설정
+	if (r + g + b < 400) {
+		h1.style.color = 'white';
+	} else {
+		h1.style.color = 'black';  // 밝은 배경에서는 검은색 텍스트로 설정
+	}
+});
+
 
 const makeRandomColor = () => {
 	const r = Math.floor(Math.random() * 256);
