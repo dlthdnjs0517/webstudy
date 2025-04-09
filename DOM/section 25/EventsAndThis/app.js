82a1df120dbee793877c7587e8551521bfcf03d5
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-09 15:53:56 +0900

    this와 generic function

--- DOM/section 25/EventsAndThis/app.js
+++ DOM/section 25/EventsAndThis/app.js
@@ -1,22 +1,23 @@
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    //내림해야 해서 256을 곱해야함
+    return `rgb(${r},${g},${b})`;
 }
 
 const buttons = document.querySelectorAll('button');
-
 for (let button of buttons) {
     button.addEventListener('click', colorize)
 }
 
 const h1s = document.querySelectorAll('h1');
+//h1은 페이지당 하나씩이 가장 적절하다.
 for (let h1 of h1s) {
     h1.addEventListener('click', colorize)
 }
 
-function colorize() {
-    this.style.backgroundColor = makeRandColor();
-    this.style.color = makeRandColor();
+function colorize() {//색을 바꾸는 generic 함수를 만든다
+    this.style.color = makeRandomColor();
+    this.style.backgroundColor = makeRandomColor();
 }
--- DOM/section 25/RandomColors/app.js
+++ DOM/section 25/RandomColors/app.js
@@ -18,10 +18,4 @@ btn.addEventListener('click', () => {
 });
 
 
-const makeRandomColor = () => {
-	const r = Math.floor(Math.random() * 256);
-	const g = Math.floor(Math.random() * 256);
-	const b = Math.floor(Math.random() * 256);
-	//내림해야 해서 256을 곱해야함
-	return `rgb(${r},${g},${b})`;
-}
+
