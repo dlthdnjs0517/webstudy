50b22a9e9d6feba465299330d901e4a226e553ae
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-13 22:08:03 +0900

    score keeper project 구조적 분해

--- DOM/Score Keeper project/app.js
+++ DOM/Score Keeper project/app.js
@@ -1,62 +1,65 @@
-const p1 = document.querySelector('#p1Btn');
-const p2 = document.querySelector('#p2Btn');
-const resetBtn = document.querySelector('#resetBtn');
-const firstSpan = document.querySelector('#firstSpan');
-const secondSpan = document.querySelector('#secondSpan');
-const scoreSelect = document.querySelector('#scoreSelect');
+// const p1 = document.querySelector('#p1Btn');
+// const p2 = document.querySelector('#p2Btn');
+// const resetBtn = document.querySelector('#resetBtn');
+// const firstSpan = document.querySelector('#firstSpan');
+// const secondSpan = document.querySelector('#secondSpan');
+// const scoreSelect = document.querySelector('#scoreSelect');
 
 
-let p1Score = 0;
-let p2Score = 0;
-let winScore = 3;
-let gameOver = false;
+// let p1Score = 0;
+// let p2Score = 0;
+// let winScore = 3;
+// let gameOver = false;
 
 
-p1.addEventListener("click", () => {
-	if (!gameOver) {
-		p1Score += 1;
-		if (p1Score === winScore) {
-			gameOver = true;
-			firstSpan.classList.add('has-text-success');
-			secondSpan.classList.add('has-text-danger');
-			p1.disabled = true;
-			p2.disabled = true;
-		}
-		firstSpan.textContent = p1Score;
-		//innerText는 display:none일 경우 무시됨.
-		//목표점수에 도달하면 클릭을 하지 못하게 설정함
-	}
-})
-p2.addEventListener("click", () => {
-	if (!gameOver) {
-		p2Score += 1;
-		if (p2Score === winScore) {
-			gameOver = true;
-			secondSpan.classList.add('has-text-success');
-			firstSpan.classList.add('has-text-danger');
-			p1.disabled = true;
-			p2.disabled = true;
-		}
-		secondSpan.textContent = p2Score;
-	}
-})
-scoreSelect.addEventListener('change', function () {
-	//!!!!화살표 함수 안에서 this는 상위 스코프의 this를 따라감!!!!(쓰면 안됨)
-	//그래서 undefined로 떴던 것
-	winScore = parseInt(this.value);
-	reset();
-})
 
-resetBtn.addEventListener('click', reset)
+// p1.addEventListener("click", function () {
 
-function reset() {
-	gameOver = false;
-	p1Score = 0;
-	p2Score = 0;
-	firstSpan.textContent = 0;
-	secondSpan.textContent = 0;
-	firstSpan.classList.remove('has-text-success', 'has-text-danger');
-	secondSpan.classList.remove('has-text-success', 'has-text-danger');
-	p1.disabled = false;
-	p2.disabled = false;
-}
+// 	updateScores(p1Score, p1Btn, firstSpan)
+// 	if (!gameOver) {
+// 		p1Score += 1;
+// 		if (p1Score === winScore) {
+// 			gameOver = true;
+// 			firstSpan.classList.add('has-text-success');
+// 			secondSpan.classList.add('has-text-danger');
+// 			p1.disabled = true;
+// 			p2.disabled = true;
+// 		}
+// 		firstSpan.textContent = p1Score;
+// 		//innerText는 display:none일 경우 무시됨.
+// 		//목표점수에 도달하면 클릭을 하지 못하게 설정함
+// 	}
+// })
+// p2.addEventListener("click", () => {
+// 	if (!gameOver) {
+// 		p2Score += 1;
+// 		if (p2Score === winScore) {
+// 			gameOver = true;
+// 			secondSpan.classList.add('has-text-success');
+// 			firstSpan.classList.add('has-text-danger');
+// 			p1.disabled = true;
+// 			p2.disabled = true;
+// 		}
+// 		secondSpan.textContent = p2Score;
+// 	}
+// })
+// scoreSelect.addEventListener('change', function () {
+// 	//!!!!화살표 함수 안에서 this는 상위 스코프의 this를 따라감!!!!(쓰면 안됨)
+// 	//그래서 undefined로 떴던 것
+// 	winScore = parseInt(this.value);
+// 	reset();
+// })
+
+// resetBtn.addEventListener('click', reset)
+
+// function reset() {
+// 	gameOver = false;
+// 	p1Score = 0;
+// 	p2Score = 0;
+// 	firstSpan.textContent = 0;
+// 	secondSpan.textContent = 0;
+// 	firstSpan.classList.remove('has-text-success', 'has-text-danger');
+// 	secondSpan.classList.remove('has-text-success', 'has-text-danger');
+// 	p1.disabled = false;
+// 	p2.disabled = false;
+// }
---
+++ DOM/Score Keeper project/app1.js
@@ -0,0 +1,66 @@
+const p1 = {
+	score: 0,
+	button: document.querySelector('#p1Btn'),
+	display: document.querySelector('#firstSpan')
+}
+const p2 = {
+	score: 0,
+	button: document.querySelector('#p2Btn'),
+	display: document.querySelector('#secondSpan')
+}
+
+
+const resetBtn = document.querySelector('#resetBtn');
+const scoreSelect = document.querySelector('#scoreSelect');
+let winScore = 3;
+let gameOver = false;
+
+function updateScores(player, opponent) {
+	if (!gameOver) {
+		player.score += 1;
+		if (player.score === winScore) {
+			gameOver = true;
+			player.display.classList.add('has-text-success');
+			opponent.display.classList.add('has-text-danger');
+			player.button.disabled = true;
+			opponent.button.disabled = true;
+		}
+		player.display.textContent = player.score;
+		//innerText는 display:none일 경우 무시됨.
+		//목표점수에 도달하면 클릭을 하지 못하게 설정함
+	}
+}
+
+
+p1Btn.addEventListener("click", function () {
+	updateScores(p1, p2);
+})
+p2Btn.addEventListener("click", function () {
+	updateScores(p2, p1);
+})
+scoreSelect.addEventListener('change', function () {
+	//!!!!화살표 함수 안에서 this는 상위 스코프의 this를 따라감!!!!(쓰면 안됨)
+	//그래서 undefined로 떴던 것
+	winScore = parseInt(this.value);
+	reset();
+})
+
+resetBtn.addEventListener('click', reset)
+
+function reset() {
+	gameOver = false;
+
+	for (let p of [p1, p2]) {
+		p.score = 0;
+		p.display.classList.remove('has-text-success', 'has-text-danger');
+		p.button.disabled = false;
+	}
+	// p1.score = 0;
+	// p2.score = 0;
+	// p1.display.textContent = 0;
+	// p2.display.textContent = 0;
+	// p1.display.classList.remove('has-text-success', 'has-text-danger');
+	// p2.display.classList.remove('has-text-success', 'has-text-danger');
+	// p1.button.disabled = false;
+	// p2.button.disabled = false;
+}
