a98c74268288a31c2110ddfe70166e58ed8eee59
dlthdnjs0517 <thdnjs0517@naver.com>
2025-04-16 20:47:44 +0900

    비동기함수 완료

--- Async 비동기/ASYNC FUNCTION/app.js
+++ Async 비동기/ASYNC FUNCTION/app.js
@@ -37,3 +37,41 @@ login('asdf')
 		console.log('error!')
 		console.log(err)
 	})
+
+
+const delayColorChange = (color, delay) => {
+	return new Promise((fulfilled, reject) => {
+		setTimeout(() => {
+			document.body.style.backgroundColor = color;
+			fulfilled();
+		}, delay)
+	})
+}
+// delayColorChange('red', 1000)
+// 	.then(() => delayColorChange('orange', 1000))
+// 	.then(() => delayColorChange('yellow', 1000))
+// 	.then(() => delayColorChange('green', 1000))
+// 	.then(() => delayColorChange('blue', 1000))
+// 	.then(() => delayColorChange('purple', 1000))
+
+async function rainbow() {
+	//await 키워드를 사용하면 지정된 시간이 지나고, 함수 실행뒤 다음 문장으로 넘어감
+	await delayColorChange('red', 1000)
+	await delayColorChange('yellow', 1000)
+	await delayColorChange('orange', 1000)
+	await delayColorChange('green', 1000)
+	await delayColorChange('blue', 1000)
+	await delayColorChange('indigo', 1000)
+	return "all done!"
+}
+
+// rainbow().then(() => console.log("end of rainbow!"))
+
+async function printRainbow() {
+	await rainbow();
+	console.log("end of rainbow!")
+}
+//console.log를 추가하면 .then을 쓰지 않아도 된다
+//await: 비동기함수(promise)가 끝날때까지 기다려주는 역할을 한다.
+//.then() : 비동기 작업 이후에 실행할 콜백을 등록
+//async: 해당 함수 내에서 await를 쓸 수 있게 한다
--- Async 비동기/promises/createPromise/app.js
+++ Async 비동기/promises/createPromise/app.js
@@ -49,3 +49,33 @@ delayColorChange('red', 1000)
 	.then(() => delayColorChange('green', 1000))
 	.then(() => delayColorChange('blue', 1000))
 	.then(() => delayColorChange('purple', 1000))
+	.then(() => delayColorChange('white', 1000))
+
+
+const fakeRequest = (url) => {//url을 받아서 Promise를 return 하는 함수
+	return new Promise((fulfilled, reject) => {
+		const delay = Math.floor(Math.random() * (4500)) + 500;
+		setTimeout(() => {
+			if (delay > 2000) {
+				reject("connection time out ;(")
+			} else {
+				fulfilled(`here is your fake data from ${url}`)
+			}
+		}, delay)
+	})
+}
+
+async function makeTwoRequests() {
+	try {
+		let data1 = await fakeRequest('/page1');
+		//fakeRequest('/page1')이 끝날때까지 기다림.
+		console.log(data1);//결과는 data1에 저장되고 출력됨.
+		let data2 = await fakeRequest('/page2');//첫요청이 끝나야 두번째 요청 시작됨
+		console.log(data2);
+	} catch (e) {//fakeRequest()중 하나라도 실패하면 catch 블록이 실행됨
+		//e는 error itself.
+		console.log("caught an error!");
+		console.log("error is:", e);
+		//promise가 reject 된 값
+	}
+}
