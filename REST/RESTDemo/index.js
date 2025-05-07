dd7ffdecdf1f62ba298b8b39b34a88b347c3c248
dlthdnjs0517 <thdnjs0517@naver.com>
2025-05-07 16:15:47 +0900

    1.uuid 패키지 설치 2. patch 함수 사용

--- REST/RESTDemo/index.js
+++ REST/RESTDemo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,18 +11,22 @@ app.set('view engine', 'ejs');
 
 const comments = [
 	{
+		id: uuid(),
 		username: 'fakermygoat',
 		comment: 'lol this is so funny!'
 	},
 	{
+		id: uuid(),
 		username: 'fakerpenguin',
 		comment: 'he is so cute'
 	},
 	{
+		id: uuid(),
 		username: 'onerluv',
 		comment: 'what the!'
 	},
 	{
+		id: uuid(),
 		username: 'geniusmonster',
 		comment: 'i am insane!'
 	},
@@ -34,8 +40,22 @@ app.get('/comments/new', (req, res) => {
 })
 app.post('/comments', (req, res) => {
 	const { username, comment } = req.body;
-	comments.push({ username, comment })
+	comments.push({ username, comment, id: uuid() })
 	res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req, res) => {
+	const { id } = req.params;
+	const comment = comments.find(c => c.id === id);
+	res.render('comments/show', { comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+	const { id } = req.params;
+	const newComment = req.body.comment;//patch 명령어에만 페이로드 포함
+	const foundComment = comments.find(c => c.id === id);
+	foundComment.comment = newComment;
+	res.redirect('/comments');
 })
 
 
