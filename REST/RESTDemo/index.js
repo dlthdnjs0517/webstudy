bdb4afabf8fb76fd92cb310048a9bcbc65ebccd1
dlthdnjs0517 <thdnjs0517@naver.com>
2025-05-07 14:58:57 +0900

    redirect 항목 추가

--- REST/RESTDemo/index.js
+++ REST/RESTDemo/index.js
@@ -1,7 +1,45 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+	{
+		username: 'fakermygoat',
+		comment: 'lol this is so funny!'
+	},
+	{
+		username: 'fakerpenguin',
+		comment: 'he is so cute'
+	},
+	{
+		username: 'onerluv',
+		comment: 'what the!'
+	},
+	{
+		username: 'geniusmonster',
+		comment: 'i am insane!'
+	},
+]
+
+app.get('/comments', (req, res) => {
+	res.render('comments/index', { comments });
+})
+app.get('/comments/new', (req, res) => {
+	res.render('comments/new');
+})
+app.post('/comments', (req, res) => {
+	const { username, comment } = req.body;
+	comments.push({ username, comment })
+	res.redirect('/comments')
+})
+
+
+
 app.get('/tacos', (req, res) => {
 	res.send('GET /tacos response')
 })
