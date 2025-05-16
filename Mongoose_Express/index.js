68b57c2c575f05613c9bafc316fa92eb644f9ad2
dlthdnjs0517 <thdnjs0517@naver.com>
2025-05-16 23:42:12 +0900

    413. product 만들기, mongoose 연동하기

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -18,11 +18,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.get('/dog', (req, res) => {
-	res.send('Woof!')
+app.get('/products', async (req, res) => {
+	const products = await Product.find({})
+	res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const product = await Product.findById(id)
+	console.log(product);
+	res.render('products/show', { product })
 })
 
 
 app.listen(8080, () => {
-	console.log('app is listening on port 3000!')
+	console.log('app is listening on port 8080!')
 })
