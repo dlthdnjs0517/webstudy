d6e98c38c28acd8f2216f53b06fd1efc68116121
dlthdnjs0517 <thdnjs0517@naver.com>
2025-05-20 17:59:33 +0900

    418. 메서드 오버라이드로 (?_method=DELETE)delete 기능 구현하기.

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -62,6 +62,12 @@ app.put('/products/:id', async (req, res) => {
 	res.redirect(`/products/${product._id}`);
 })
 
+app.delete('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const deletedProduct = await Product.findByIdAndDelete(id);
+	res.redirect('/products');
+
+})
 
 app.listen(8080, () => {
 	console.log('app is listening on port 8080!')
