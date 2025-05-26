431e17a4095f852e92fee9e9e7d09e7d98e3d369
dlthdnjs0517 <31719831+dlthdnjs0517@users.noreply.github.com>
2025-05-26 13:41:31 +0900

    Update index.js
    mongodb token leaked issue solved

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
-mongoose.connect('mongodb+srv://Admin:mlPjdzKISG3wQ9PC@udemy-lecutre.hlt9efv.mongodb.net/farmStand?retryWrites=true&w=majority&appName=Udemy-lecutre')
+mongoose.connect('mongodb+srv://@udemy-lecutre.hlt9efv.mongodb.net/farmStand?retryWrites=true&w=majority&appName=Udemy-lecutre')
 	// 노트북 사양 때문에 atlas 사용중이라서 위와같은 url 사용해야함.
 	.then(() => {
 		console.log('Mongo connection open!')
